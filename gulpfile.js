48b6a519708832a9ea9de2301209d6ae9e650375
wilson <wangxiang@rongyi.com>
2017-08-24 18:50:34 +0800

    update

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,8 @@
 /*
 * @Author: Wei Jie
 * @Date:   2017-04-19 10:34:34
-* @Last Modified by:   Wei Jie
-* @Last Modified time: 2017-04-25 15:16:19
+* @Last Modified by:   wangxiang
+* @Last Modified time: 2017-08-24 18:50:16
 */
 
 'use strict';
@@ -20,10 +20,9 @@ var sftp = require('gulp-sftp');
 function runPublish(hj) {
     return gulp.src(hj + '/**')
         .pipe(sftp({
-            host: '101.69.176.99',
-            user: 'jfuser',
-            pass: 'GAmPckKIit6JDHqOafMA',
-            // remotePath: '/home/jfuser/tt',
-            remotePath: '/var/www/html/v35/' + hj
+            host: '',
+            user: '',
+            pass: '',
+            remotePath: '' + hj
         }));
 }
