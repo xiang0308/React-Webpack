c046f4703c0efbce640be9fa28f44a1f607f60f3
wilson <wangxiang@rongyi.com>
2017-08-24 18:52:02 +0800

    update

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,18 @@
 /*
-* @Author: Wei Jie
-* @Date:   2017-04-19 10:34:34
-* @Last Modified by:   wangxiang
-* @Last Modified time: 2017-08-24 18:50:16
-*/
-
+ * @Author: wangxiang
+ * @Date:   2017-04-19 10:34:34
+ * @Last Modified by:   wangxiang
+ * @Last Modified time: 2017-08-24 18:51:39
+ */
 'use strict';
 var gulp = require('gulp');
 var sftp = require('gulp-sftp');
 
-
 ['rd', 'qa', 'dist'].forEach(function(item) {
-    gulp.task('sftp-' + item, function () {
+    gulp.task('sftp-' + item, function() {
         runPublish(item);
     });
 });
-
 
 function runPublish(hj) {
     return gulp.src(hj + '/**')
